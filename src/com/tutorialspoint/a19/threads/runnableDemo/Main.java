decc27c829a6b465a2d6b9180de6e3d5aae1b18e
achujelas123 <achujel3@gmail.com>
2017-03-20 18:12:48 +0200

    testing some threading

---
+++ src/com/tutorialspoint/a19/threads/runnableDemo/Main.java
@@ -0,0 +1,15 @@
+package com.tutorialspoint.a19.threads.runnableDemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        RunnableDemo R1 = new RunnableDemo("Thread-1");
+        R1.start();
+
+        RunnableDemo R2 = new RunnableDemo("Thread-2");
+        R2.start();
+
+    }
+
+}
